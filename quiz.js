a993ef26bf862379e561d4f73a104423bc68c41d
swygen <swygenofficial@gmail.com>
2025-05-15 16:29:57 +0600

    Create quiz.js

---
+++ quiz.js
@@ -0,0 +1,44 @@
+[
+  {
+    "subject": "বাংলা",
+    "question": "‘অতিথি’ শব্দের বিপরীত শব্দ কী?",
+    "options": ["মালিক", "অজানা", "স্থায়ী", "বন্ধু"],
+    "answer": "স্থায়ী"
+  },
+  {
+    "subject": "ইংরেজি",
+    "question": "Choose the correct synonym for 'happy':",
+    "options": ["angry", "joyful", "sad", "tired"],
+    "answer": "joyful"
+  },
+  {
+    "subject": "গণিত",
+    "question": "5 + 3 × 2 = ?",
+    "options": ["16", "11", "13", "10"],
+    "answer": "11"
+  },
+  {
+    "subject": "আইসিটি",
+    "question": "CPU এর পূর্ণরূপ কী?",
+    "options": ["Central Performance Unit", "Central Processing Unit", "Computer Process Unit", "Control Processing Unit"],
+    "answer": "Central Processing Unit"
+  },
+  {
+    "subject": "বিজ্ঞান",
+    "question": "পানির রাসায়নিক সংকেত কী?",
+    "options": ["O2", "H2", "H2O", "HO2"],
+    "answer": "H2O"
+  },
+  {
+    "subject": "ইসলাম",
+    "question": "ইসলামের পঞ্চ স্তম্ভের মধ্যে প্রথমটি কী?",
+    "options": ["নামাজ", "রোজা", "কালেমা", "হজ"],
+    "answer": "কালেমা"
+  },
+  {
+    "subject": "ইতিহাস",
+    "question": "বাংলাদেশ স্বাধীন হয় কত সালে?",
+    "options": ["১৯৭১", "১৯৭৫", "১৯৪৭", "১৯৫২"],
+    "answer": "১৯৭১"
+  }
+]
