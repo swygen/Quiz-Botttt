b30a1b1d8d715c3f65bb2bc40db5d6d3a12e2799
swygen <swygenofficial@gmail.com>
2025-05-15 16:28:03 +0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+export const BOT_TOKEN = '8075611705:AAHIUcDb4fP55yzvHJVx7dj7KJdrOLQlLp0';
+export const GROUP_ID = -1002572781690; // তোমার Telegram গ্রুপ ID
+export const GROUP_LINK = 'https://t.me/swygenbd';
